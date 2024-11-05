232afcaa300bc2623d624b62dc4d3e70df394b58
meli-huang <meli.huang05@gmail.com>
2024-11-04 22:02:59 -0600

    finished week 3 homework

--- Week5/pokemon-app/src/App.js
+++ Week5/pokemon-app/src/App.js
@@ -4,6 +4,8 @@ import { ThemeProvider } from '@mui/material/styles';
 import { ThemeContext } from './context/ThemeContext';
 import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
 import { WbSunny, NightsStay } from '@mui/icons-material';
+import PokemonList from './components/PokemonList.js';
+import PokemonDetail from './components/PokemonDetail.js';
 
 function App() {
 
@@ -11,7 +13,28 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      Hello World!
+      
+      {/* Components & classes provided by MUI, or Material-UI */}
+      <AppBar position='sticky'>
+        <Toolbar>
+          {/* flexGrow makes typography grow to biggest avail space */}
+          <Typography variant='h6' sx={{flexGrow: 1}}> 
+            Pokedex App
+          </Typography>
+
+          {/* Toggle the icon based on global theme using toggleTheme function */}
+          <IconButton onClick={toggleTheme}>
+            {theme.palette.mode === 'dark' ? <WbSunny/> : <NightsStay/>}
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+
+      <Routes>
+        <Route path='/' element={<PokemonList/>}/>
+        <Route path='/:name' element={<PokemonDetail/>}/>
+      </Routes>
+
     </ThemeProvider>
   );
 }
--- Week5/pokemon-app/src/components/PokemonList.js
+++ Week5/pokemon-app/src/components/PokemonList.js
@@ -1,13 +1,30 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Grid2 } from "@mui/material";
+import PokemonCard from "./PokemonCard.js"
 
 function PokemonList() {
 
+  const [pokemons, setPokemons] = useState([]);
+
+  function fetchPokemon() {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151")
+    .then((response) => {
+      setPokemons(response.data.results)
+      console.log(response.data.results)
+    })
+  }
+
+  useEffect(() => {
+    fetchPokemon();
+  }, [])
+
   return (
-    <div>
-        Hello World! This is supposed to show the entire list of Pokemons.
-    </div>
+    <Grid2 container justifyContent="center">
+      {pokemons.map((pokemon, index) => (
+        <PokemonCard key={pokemon.name} pokemon={pokemon} index={index + 1}/>
+      ))}
+    </Grid2>
   );
 }
 
